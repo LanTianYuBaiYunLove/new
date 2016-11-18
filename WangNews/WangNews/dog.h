f41376dc3a76460c5d700fa7c0df41b3a5e95ce8
GY <2916979411@qq.com>
2016-11-18 16:17:13 +0800

    创建了 dog 类

---
+++ WangNews/WangNews/dog.h
@@ -0,0 +1,13 @@
+//
+//  dog.h
+//  WangNews
+//
+//  Created by lanou3g on 16/11/18.
+//  Copyright © 2016年 com.666it. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface dog : NSObject
+
+@end
---
+++ WangNews/WangNews/dog.m
@@ -0,0 +1,13 @@
+//
+//  dog.m
+//  WangNews
+//
+//  Created by lanou3g on 16/11/18.
+//  Copyright © 2016年 com.666it. All rights reserved.
+//
+
+#import "dog.h"
+
+@implementation dog
+
+@end

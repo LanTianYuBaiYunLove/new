7d02ab6252d5f8317589f1d169688fdc5910344c
XMG <XMG@163.com>
2016-11-18 16:07:34 +0800

    初始化项目

---
+++ WangNews/WangNews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WangNews
+//
+//  Created by lanou3g on 16/11/18.
+//  Copyright © 2016年 com.666it. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WangNews/WangNews/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WangNews
+//
+//  Created by lanou3g on 16/11/18.
+//  Copyright © 2016年 com.666it. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

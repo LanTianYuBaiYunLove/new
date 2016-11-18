8db1b9253b50901bf7ae9c01942b8135cd11b52f
XMG <XMG@163.com>
2016-11-18 19:03:31 +0800

    7898787

---
+++ WangNews/WangNews/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  WangNews
+//
+//  Created by lanou3g on 16/11/18.
+//  Copyright © 2016年 com.666it. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
